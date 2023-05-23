bfa95dc9c1d0d0efebdae72d3a7ea200e7f2da3f
saparatayev <sopa.97@mail.ru>
2023-05-23 17:55:15 +0500

    reorder operation

--- app/Http/Controllers/Admin/CategoryCrudController.php
+++ app/Http/Controllers/Admin/CategoryCrudController.php
@@ -18,6 +18,16 @@ class CategoryCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ReorderOperation;
+
+    protected function setupReorderOperation()
+    {
+        // define which model attribute will be shown on draggable elements 
+        $this->crud->set('reorder.label', 'title');
+        // define how deep the admin is allowed to nest the items
+        // for infinite levels, set it to 0
+        $this->crud->set('reorder.max_level', 3);
+    }
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
