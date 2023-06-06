2ab7c29072c967e4be34d3a61f15d2719e0e91d5
saparatayev <sopa.97@mail.ru>
2023-06-06 12:03:28 +0500

    added localization by documentation

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\HasTranslations;
+use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\Sluggable;
+use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\SluggableScopeHelpers;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,6 +15,8 @@ class Article extends Model
     use CrudTrait;
     use HasFactory;
     use SoftDeletes;
+    use Sluggable, SluggableScopeHelpers;
+    use HasTranslations;
 
     protected $fillable = [
         'title',
@@ -29,9 +34,33 @@ class Article extends Model
         'metas' => 'array',
     ];
 
+    protected $translatable = [
+        'title',
+        'slug',
+        'description'
+    ];
+
     public function tags()
     {
         return $this->belongsToMany('App\Models\Tag', 'article_tag');
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true,
+                'onCreate' => true,
+                'translatable' => true, // Enable multilingual slugs
+                'translatable_separator' => '-', // Separator for translated slugs
+            ],
+        ];
     }
 }
 
